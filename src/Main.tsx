a9a53981df78b54eab57b9cd79c725a2d54a1b30
Pavel Stepanov <115621772+madbrine@users.noreply.github.com>
2023-12-14 23:03:53 +0900

    upd

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,13 @@
 import Hex from "./components/Hex";
 import useWindowDimensions from "./hooks/useWindowDimensions";
 import HexagonsScreen from "./screens/HexagonsScreen";
+import SceneRenderer from "./screens/SceneRenderer";
 
 function Main() {
   return (
-    <div style={{position: 'absolute', padding: 12}}>
-      <HexagonsScreen hexSize={50} hexBorder={5}/>
+    <div style={{position: 'absolute'}}>
+      {/* <HexagonsScreen hexSize={50} hexBorder={5}/> */}
+      <SceneRenderer hexSize={50} hexBorder={5}/>
     </div>
   );
 }
---
+++ src/components/HexShape.tsx
@@ -0,0 +1,65 @@
+import { Shape } from "react-konva";
+
+interface IProps {
+    border: number,
+    size: number,
+    borderColor: string,
+    hexColor: string,
+    hexEdge: number,
+    xPosition: number,
+    yPosition: number,
+}
+function HexShape({
+    border,
+    size,
+    borderColor,
+    hexColor,
+    hexEdge,
+    xPosition,
+    yPosition,
+}: IProps) {
+    const xp = xPosition * size;
+    const yp = size * 0.75 * yPosition;
+    return (
+        <Shape
+            sceneFunc={(context, shape) => {
+                context.beginPath();
+                context.moveTo(
+                    xp,
+                    yp + 0.25 * size
+                );
+                context.lineTo(
+                    xp + 0.5 * size,
+                    yp
+                );
+                context.lineTo(
+                    xp + size,
+                    yp + 0.25 * size
+                );
+                context.lineTo(
+                    xp + size,
+                    yp + 0.75 * size
+                );
+                context.lineTo(
+                    xp + 0.5 * size,
+                    yp + size
+                );
+                context.lineTo(
+                    xp,
+                    yp + 0.75 * size
+                );
+                context.lineTo(
+                    xp,
+                    yp + 0.25 * size
+                );
+                context.closePath();
+                // (!) Konva specific method, it is very important
+                context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="gray"
+            strokeWidth={4}
+        />);
+}
+
+export default HexShape;
